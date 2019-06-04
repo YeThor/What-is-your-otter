c89e379cbc6301305d90848b035e7327000c2bda
YeThor <kysloves@naver.com>
2019-06-04 23:17:50 +0900

    ✨ <QuizPage/>, <ResultPage/> 라우팅 연결

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import IntroPage from "./components/IntroPage.jsx";
 import "./App.css";
+import QuizPage from "./components/QuizPage.jsx";
+import ResultPage from "./components/ResultPage.jsx";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={IntroPage} />
+        <Route path="/quiz" component={QuizPage} />
+        <Route path="/result" component={ResultPage} />
       </div>
     );
   }
