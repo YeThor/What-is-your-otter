93bb9456b7ec9e0839b9be62e02bb634df5d79eb
YeThor <kysloves@naver.com>
2019-06-04 22:56:50 +0900

    ✨ 현재 구조에 react-routing 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import IntroPage from "./components/IntroPage.jsx";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return <IntroPage />;
+    return (
+      <div>
+        <Route exact path="/" component={IntroPage} />
+      </div>
+    );
   }
 }
 
