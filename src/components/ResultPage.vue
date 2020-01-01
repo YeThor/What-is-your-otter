53acc140d72f435288f4d9c7551e676e37867d81
YeThor <kysloves@naver.com>
2020-01-02 01:10:09 +0900

    ✨ 기본 Vue 라우터 세팅
    홈페이지 -> 퀴즈 페이지 -> 결과 페이지 -> 홈페이지 순으로 기본 라우팅 설정

---
+++ src/components/ResultPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Result Page</h1>
+    <router-link to="/">다시 하기</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultPage"
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import IntroPage from "./components/IntroPage.vue";
+import QuizPage from "./components/QuizPage.vue";
+import ResultPage from "./components/ResultPage.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: IntroPage },
+  { path: "/quiz", component: QuizPage },
+  { path: "/result", component: ResultPage }
+];
+
+const router = new VueRouter({ routes });
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
