3e0b8db4ac53e684676375d4c5dedb5201e11b84
YeThor <kysloves@naver.com>
2020-01-04 23:43:53 +0900

    :lipstick: 결과 페이지 vuetify 적용

--- src/components/ResultPage.vue
+++ src/components/ResultPage.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="10">
+        <v-row justify="center">
+          <v-img :src="this.sudal.url" max-width="500" min-width="250" />
+        </v-row>
+
+        <v-row justify="center">
+          <v-card shaped max-width="500">
+            <v-card-title class="headline"
+              >당신은 {{ this.sudal.name }}이군요!</v-card-title
+            >
+            <v-card-text>{{ this.sudal.description }}</v-card-text>
+            <v-card-actions>
+              <v-btn text color="deep-purple accent-4" @click="showNote">
+                창작자 노트
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <div>
     <h1>당신은 {{ this.sudal.name }} 입니다!</h1>
     <img :src="this.sudal.url" />
     <p>{{ this.sudal.description }}</p>
@@ -8,13 +31,19 @@
       <p>창작자 한마디: {{ this.sudal.creator.note }}</p>
     </div>
     <router-link to="/">다시 하기</router-link>
-  </div>
+  </div> -->
 </template>
 
 <script>
 export default {
   name: "ResultPage",
-  props: ["sudal"]
+  props: ["sudal"],
+  methods: {
+    showNote: function() {
+      // @TODO: 창작자 노트는 어떻게?
+      alert("hi");
+    }
+  }
 };
 </script>
 
