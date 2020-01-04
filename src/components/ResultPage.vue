90965ed3d6f7e81d7a39d2cac135bfe5c84985c0
YeThor <kysloves@naver.com>
2020-01-05 00:42:05 +0900

    :sparkles: 창작자 노트 확장형 형태로 추가

--- src/components/ResultPage.vue
+++ src/components/ResultPage.vue
@@ -12,11 +12,19 @@
               >당신은 {{ this.sudal.name }}이군요!</v-card-title
             >
             <v-card-text>{{ this.sudal.description }}</v-card-text>
-            <v-card-actions>
-              <v-btn text color="deep-purple accent-4" @click="showNote">
-                창작자 노트
-              </v-btn>
-            </v-card-actions>
+            <v-expansion-panels>
+              <v-expansion-panel>
+                <v-expansion-panel-header class="deep-purple--text">
+                  창작자 노트...
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <p class="font-italic subtitle-2">
+                    created by {{ this.sudal.creator.name }}
+                  </p>
+                  <p class="body-2">"{{ this.sudal.creator.note }}"</p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-card>
         </v-row>
       </v-col>
